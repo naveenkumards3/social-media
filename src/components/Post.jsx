2c43a18773811d4bf8103803bb7c877fefb05134
naveenkumards3 <naveends8228@gmail.com>
2024-09-23 10:44:38 +0530

    first commit

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,10 +9,9 @@ const Post = ({ post }) => {
       <div className="card-body">
         <h5 className="card-title">
           {post.title}
-          <span
-            className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
-            onClick={() => deletePost(post.id)}
-          ></span>
+          <span className="remove" onClick={() => deletePost(post.id)}>
+            <i class="bi bi-trash remove"></i>
+          </span>
         </h5>
         <p className="card-text">{post.body}</p>
         {post.tags.map((tag) => (
